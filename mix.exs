7fcbb1a2dbe682fc72649b3c31bcfbfb256e92f5
Laurent GAY <l.gay@sd-libre.fr>
2026-07-23 17:21:17 +0200

    Bump version from 1.3.4.1 to 1.3.5

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Rajska.MixProject do
   def project do
     [
       app: :rajska,
-      version: "1.3.4.1",
+      version: "1.3.5",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
